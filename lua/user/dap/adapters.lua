8f5932e139e8182004e08dffbed6b60de4e7af50
Garrett Credi [ddxtanx] <gcc@ameritech.net>
2022-12-31 15:34:33 -0600

    Add debug setup, REQUIRES vscode-lldbgit add ./

--- lua/user/dap/adapters.lua
+++ lua/user/dap/adapters.lua
@@ -1,7 +1,14 @@
+code_lldb_port_num = 13000
+
 return {
-  codelldb = {
+  lldb = {
     type = 'server',
+    executable = { 
+      command = '/Users/Garrett_Credi/.vscode/extensions/vadimcn.vscode-lldb-1.8.1/adapter/codelldb',
+      args = {"--port", code_lldb_port_num}-- adjust as needed, must be absolute path
+    },
+    name = 'lldb',
     host = '127.0.0.1',
-    port = 13000
-  },
+    port = code_lldb_port_num
+  }
 }
