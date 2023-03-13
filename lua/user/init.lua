1da7be7b9f214f6f3a5c8815ef32def9ce97b96a
Garrett Credi [ddxtanx] <gcc@ameritech.net>
2023-03-13 01:11:31 -0500

    Mess w startup order, make startup time quicker

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -224,20 +224,6 @@ local config = {
       ft = "md"
     },
     {
-      'kkoomen/vim-doge',
-      init = function()
-        vim.g.doge_mapping = "<Leader>D"
-      end,
-      build = function()
-        vim.fn["doge#install"]()
-        vim.g.doge_mapping = "<Leader>D"
-      end,
-      config = function()
-        vim.g.doge_mapping = "<Leader>D"
-      end,
-      event = "BufEnter"
-    },
-    {
       "ray-x/lsp_signature.nvim",
       config = function() require("lsp_signature/setup") end,
       event = "BufEnter"
@@ -258,11 +244,11 @@ local config = {
         'nvim-lua/plenary.nvim',
         'nvim-telescope/telescope.nvim'
       },
-      event = "BufEnter"
+      keys = "<leader>v"
     },
     {
       "simrat39/inlay-hints.nvim",
-      event = "BufEnter"
+      ft = "rs"
     },
     {
       "simrat39/rust-tools.nvim",
@@ -271,7 +257,7 @@ local config = {
         "simrat39/inlay-hints.nvim",
       },
       config = function() require("rust_tools/rust_tools") end,
-      event = "BufEnter"
+      ft = "rs"
     },
     {
       'SirVer/ultisnips'
@@ -326,7 +312,60 @@ local config = {
       event = "BufEnter"
     },
     'folke/tokyonight.nvim',
-    require("user/astrocommunity")
+    require("user/astrocommunity"),
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter"
+      },
+      event = "BufEnter",
+      config = function() 
+        require'nvim-treesitter.configs'.setup {
+          textobjects = {
+            select = {
+              enable = true,
+
+              -- Automatically jump forward to textobj, similar to targets.vim
+              lookahead = true,
+
+              keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                -- nvim_buf_set_keymap) which plugins like which-key display
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                -- You can also use captures from other query groups like `locals.scm`
+                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+              },
+              -- You can choose the select mode (default is charwise 'v')
+              --
+              -- Can also be a function which gets passed a table with the keys
+              -- * query_string: eg '@function.inner'
+              -- * method: eg 'v' or 'o'
+              -- and should return the mode ('v', 'V', or '<c-v>') or a table
+              -- mapping query_strings to modes.
+              selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = '<c-v>', -- blockwise
+              },
+              -- If you set this to `true` (default is `false`) then any textobject is
+              -- extended to include preceding or succeeding whitespace. Succeeding
+              -- whitespace has priority in order to act similarly to eg the built-in
+              -- `ap`.
+              --
+              -- Can also be a function which gets passed a table with the keys
+              -- * query_string: eg '@function.inner'
+              -- * selection_mode: eg 'v'
+              -- and should return true of false
+              include_surrounding_whitespace = true,
+            },
+          },
+        }
+      end
+    }
     -- {
     --   "saecki/crates.nvim",
     --   tag = "v0.3.0",
