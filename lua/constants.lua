bfd72a0a1be5683fc09814602f3fb4a3c32c160c
Garrett Credi [ddxtanx] <gcc@ameritech.net>
2023-03-28 01:06:51 -0500

    Add tex template

--- lua/constants.lua
+++ lua/constants.lua
@@ -1,5 +1,4 @@
-vim.g.vimtex_view_general_viewer = 'okular'
-vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+vim.g.vimtex_view_method = 'skim'
 vim.cmd([[
   set conceallevel=1
 ]])
