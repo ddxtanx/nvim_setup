a2c1a43e13d501a1126e66cf5c94fd0eab71c3bb
Garrett Credi <garrettcredi@gmail.com>
2023-08-22 02:23:24 -0500

    new discodd presence

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -293,10 +293,29 @@ local config = {
       event = "BufEnter",
     },
     {
-      "andweeb/presence.nvim",
-      config = function() require "presence/setup" end,
-      lazy = false,
-    },
+    "andweeb/presence.nvim",
+    lazy = false,
+    config = function()
+      require("presence").setup({
+        auto_update         = true,
+        neovim_image_text   = ">>>Emacs",
+        main_image          = "neovim",
+        log_level           = "debug",
+        debounce_timeout    = 10,
+        enable_line_number  = true,
+        buttons             = true,
+        show_time           = true,
+        -- Rich Presence text options
+        editing_text        = "Editing %s",
+        file_explorer_text  = "Browsing %s",
+        git_commit_text     = "Committing changes",
+        plugin_manager_text = "Managing plugins",
+        reading_text        = "Reading %s",
+        workspace_text      = "Working on %s",
+        line_number_text    = "Line %s out of %s"
+    })
+    end
+    },   
     "quangnguyen30192/cmp-nvim-ultisnips",
     { -- override nvim-autopairs plugin
       "hrsh7th/nvim-cmp",
