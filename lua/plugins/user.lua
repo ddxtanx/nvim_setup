c34aae37416a91c31264ddf0182f4a2a5a5c9513
Garrett Credi <garrettcredi@gmail.com>
2024-06-28 03:58:14 -0500

    add neocord

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -188,28 +188,8 @@ return {
     setup = function() require("fundo").setup() end,
   },
   {
-    "andweeb/presence.nvim",
-    enabled = true,
-    lazy = false,
-    config = function()
-      require("presence").setup {
-        auto_update = true,
-        neovim_image_text = ">>>Emacs",
-        main_image = "neovim",
-        debounce_timeout = 10,
-        enable_line_number = true,
-        buttons = true,
-        show_time = true,
-        -- Rich Presence text options
-        editing_text = "Editing %s",
-        file_explorer_text = "Browsing %s",
-        git_commit_text = "Committing changes",
-        plugin_manager_text = "Managing plugins",
-        reading_text = "Reading %s",
-        workspace_text = "Working on %s",
-        line_number_text = "Line %s out of %s",
-      }
-    end,
+    "IogaMaster/neocord",
+    event = "VeryLazy",
   },
   { "nvim-neotest/nvim-nio" },
   {
