8b85c38795c1c7bed9c7d4e6aa6caf852012f33e
Garrett Credi (ddxtanx) <garrettcredi@gmail.com>
2024-12-17 20:21:15 -0600

    rust debug

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,16 +1,51 @@
-vim.cmd([[
+vim.cmd [[
 tnoremap("<esc>", "<C-\><C-n>", opts)
 tnoremap('jk', "<C-\><C-n>", opts)
 tnoremap('<C-h>', "<C-\><C-n><C-W>h", opts)
 tnoremap('<C-j>', "<C-\><C-n><C-W>j", opts)
 tnoremap('<C-k>', "<C-\><C-n><C-W>k", opts)
 tnoremap('<C-l>', "<C-\><C-n><C-W>l", opts)
-]])
+]]
 
-vim.keymap.set("n", "<Leader>dC", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>", { desc = "Set a conditional breakpoint" })
-vim.keymap.set("n", "<Leader>dE", ":lua require'dap'.set_exception_breakpoints({'all'})<cr>", { desc = "Set an exception breakpoint"})
-vim.keymap.set("n", "<leader>dV", ":lua require('dap.ext.vscode').load_launchjs(nil, { lldb = {'c', 'cpp', 'rust' }, cppdbg = {'c', 'cpp'} })<cr>", { desc = "Loads a launch.json config for debugging"})
-vim.keymap.set("n", "<leader>vt", ":lua require('telescope').extensions.vstask.tasks()<cr>", { desc = "View launch.json tasks"})
-vim.keymap.set("n", "<leader>vi", ":lua require('telescope').extensions.vstask.inputs()<cr>", { desc = "View launch.json inputs"})
-vim.keymap.set("n", "<leader>vh", ":lua require('telescope').extensions.vstask.history()<cr>", { desc = "View launch.json task history"})
-vim.keymap.set("n", "<leader>vl", ":lua require('telescope').extensions.vstask.launch()<cr>", { desc = "Launch task.json tasks"})
+vim.keymap.set(
+  "n",
+  "<Leader>dC",
+  ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
+  { desc = "Set a conditional breakpoint" }
+)
+vim.keymap.set(
+  "n",
+  "<Leader>dE",
+  ":lua require'dap'.set_exception_breakpoints({'all'})<cr>",
+  { desc = "Set an exception breakpoint" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>dV",
+  ":lua require('dap.ext.vscode').load_launchjs(nil, { lldb = {'c', 'cpp', 'rust' }, cppdbg = {'c', 'cpp'} })<cr>",
+  { desc = "Loads a launch.json config for debugging" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>vt",
+  ":lua require('telescope').extensions.vstask.tasks()<cr>",
+  { desc = "View launch.json tasks" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>vi",
+  ":lua require('telescope').extensions.vstask.inputs()<cr>",
+  { desc = "View launch.json inputs" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>vh",
+  ":lua require('telescope').extensions.vstask.history()<cr>",
+  { desc = "View launch.json task history" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>vl",
+  ":lua require('telescope').extensions.vstask.launch()<cr>",
+  { desc = "Launch task.json tasks" }
+)
--- lua/plugins/dap/configurations.lua
+++ lua/plugins/dap/configurations.lua
@@ -1,37 +1,27 @@
 cpp_conf = {
-    name = 'VSCode LLDB Debugger',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
+  name = "VSCode LLDB Debugger",
+  type = "lldb",
+  request = "launch",
+  program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+  cwd = "${workspaceFolder}",
+  stopOnEntry = false,
+  args = {},
 
-    -- 💀
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    -- runInTerminal = false,
+  -- 💀
+  -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+  --
+  --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+  --
+  -- Otherwise you might get the following error:
+  --
+  --    Error on launch: Failed to attach to the target process
+  --
+  -- But you should be aware of the implications:
+  -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+  -- runInTerminal = false,
 }
 
-rust_conf = cpp_conf
-
-rust_conf['program'] = function()
-  vim.fn.system('cargo build')
-  return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-end
-
 return {
-  cpp = {cpp_conf},
-  c = {cpp_conf},
-  rust = {rust_conf}
+  cpp = { cpp_conf },
+  c = { cpp_conf },
 }
