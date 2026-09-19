7a04fb8d759b2e07c3968531b2a275fce152d42d
Garrett Credi <garrettcredi@gmail.com>
2026-09-19 15:56:11 -0500

    osc52

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -40,11 +40,12 @@ return {
       opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        spell = true, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
       },
       g = { -- vim.g.<key>
+        clipboard = "osc52",
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
